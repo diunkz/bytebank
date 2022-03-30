65b8ab217dd77698abf5257e9d3fdc285db861f9
Junkes Junior <junkesjunior@live.com>
2022-03-30 01:00:40 -0400

    Showing list of transfers with data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
-  transferencia: any;
+  transferencias: any[] = [];
   // destino: number;
   // valor: number;
 
   transferir($event) {
     console.log($event);
-    this.transferencia = $event;
+    const transferencia = { ...$event, data: new Date() };
+    this.transferencias.push(transferencia);
     // this.destino = $event.destino;
     // this.valor = $event.valor;
   }
